c3913c554305e0fd8647266f26c832854d40233e
bamjam101 <herejamil123@gmail.com>
2023-01-24 13:23:26 +0530

    Created Home page structure using Material UI components.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,42 @@
 import React from "react";
 import { ThemeProvider } from "@emotion/react";
-import { createTheme, CssBaseline } from "@mui/material";
+import { createTheme, CssBaseline, useMediaQuery } from "@mui/material";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
 
-const theme = createTheme({
-  palettle: {
-    mode: "light",
-  },
-});
-
 const Layout = () => {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const customPalette = (mode) => ({
+    palette: {
+      mode,
+      ...(mode === "light"
+        ? {
+            primary: "#d9d9d9d9",
+            divider: "#d9d9d9"[200],
+            text: {
+              primary: "#808080bf"[900],
+              secondary: "#8080807f"[800],
+            },
+          }
+        : {
+            primary: "#191919",
+            divider: "#191919"[700],
+            background: {
+              default: "#191919"[900],
+              paper: "#191919"[900],
+            },
+            text: {
+              primary: "#a4a4a4",
+              secondary: "#c4c4c4"[500],
+            },
+          }),
+    },
+  });
+  const theme = createTheme({
+    palette: {
+      mode: prefersDarkMode ? "dark" : "light",
+    },
+  });
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,97 @@
+import { useTheme } from "@emotion/react";
+import { ShoppingCartRounded } from "@mui/icons-material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  IconButton,
+  Rating,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const theme = useTheme();
+  const [products, setProducts] = useState([]);
+  async function fetchAllProducts() {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const result = await response.json();
+    setProducts(result);
+  }
+
+  useEffect(() => {
+    fetchAllProducts();
+  }, []);
+  return (
+    <Container sx={{ py: 8 }} maxWidth="lg">
+      <Grid container spacing={4}>
+        {products?.map(({ title, id, image, price, description, rating }) => (
+          <Grid item key={id} xs={12} sm={6} md={3}>
+            <Card
+              sx={{ height: "100%", display: "flex", flexDirection: "column" }}
+            >
+              <CardMedia
+                component="img"
+                sx={{
+                  alignSelf: "center",
+                  widht: theme.spacing(30),
+                  height: theme.spacing(30),
+                  objectFit: "contain",
+                  paddingTop: theme.spacing(),
+                  background: "white",
+                }}
+                image={image}
+                alt={`${title} Image`}
+              />
+              <CardContent>
+                <Typography
+                  variant="h5"
+                  component="h2"
+                  gutterBottom
+                  sx={{
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    display: "-webkit-box",
+                    "-webkit-line-clamp": "1",
+                    "-webkit-box-orient": "vertical",
+                  }}
+                >
+                  {title}
+                </Typography>
+                <Typography
+                  paragraph
+                  color={"text.secondary"}
+                  sx={{
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    display: "-webkit-box",
+                    "-webkit-line-clamp": "2",
+                    "-webkit-box-orient": "vertical",
+                  }}
+                >
+                  {description}
+                </Typography>
+                <Typography paragraph>{`$ ${price}`}</Typography>
+                <Rating readOnly precision={0.5} value={rating.rate} />
+              </CardContent>
+              <CardActions sx={{ alignSelf: "center" }}>
+                <Button size="medium">
+                  <ShoppingCartRounded />
+                  Add To Cart
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 };
 
 export default Home;
